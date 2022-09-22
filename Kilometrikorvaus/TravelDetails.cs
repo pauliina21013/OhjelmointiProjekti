d1a4ed18efc8b7f0986a4c5bfbdff69dcb5e6d11
Pessi Vilppolahti <pessiv@outlook.com>
2022-09-22 15:42:52 +0300

    made a few functions to the traveldetails class

--- Kilometrikorvaus/TravelDetails.cs
+++ Kilometrikorvaus/TravelDetails.cs
@@ -4,14 +4,49 @@
     {
         private string timeOfLeaving = "01/01/1999 00:00";
         private string timeOfReturning = "01/01/1999 00:00";
-        private List<int> kilometersTravelled = new List<int>();
-        private List<int> hoursTravelled = new List<int>();
-        public List<List<int>> tripLengthVariablesInAList()
+        private List<int> kilometersTravelled = new();
+        private List<int> hoursTravelled = new();
+        private string dateOfInput = "";
+        public List<List<int>> TripLengthVariablesInAList()
         {
-            List<List<int>> trip = new List<List<int>>();
-            trip.Add(kilometersTravelled);
-            trip.Add(hoursTravelled);
+            List<List<int>> trip = new()
+            {
+                kilometersTravelled,
+                hoursTravelled
+            };
             return trip;
+        }
+        public void AddKilometersTravelledInAList(int kilometers)
+        {
+            kilometersTravelled.Add(kilometers);
+        }
+        public void AddHoursTravelledInAList(int hours)
+        {
+            hoursTravelled.Add(hours);
+        }
+        public string GetTimeOfLeaving()
+        {
+            return timeOfLeaving;
+        }
+        public void SetTimeOfLeaving(string timeOfLeaving)
+        {
+            this.timeOfLeaving = timeOfLeaving;
+        }
+        public string GetTimeOfReturning()
+        {
+            return timeOfReturning;
+        }
+        public void SetTimeOfReturning(string timeOfReturning)
+        {
+            this.timeOfReturning = timeOfReturning;
+        }
+        public void SetDateOfInput()
+        {
+            dateOfInput = DateTime.Now.ToShortDateString();
+        }
+        public string GetDateOfInput()
+        {
+            return dateOfInput;
         }
     }
 }
