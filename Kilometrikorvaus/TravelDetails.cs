121e4dbf01172302e0418d7a312b58e5e8b22de0
Pessi Vilppolahti <pessiv@outlook.com>
2022-09-22 14:54:49 +0300

    created a class for the trip details

---
+++ Kilometrikorvaus/TravelDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace Kilometrikorvaus
+{
+    internal class TravelDetails
+    {
+        private string timeOfLeaving = "01/01/1999 00:00";
+        private string timeOfReturning = "01/01/1999 00:00";
+        private List<int> kilometersTravelled = new List<int>();
+        private List<int> hoursTravelled = new List<int>();
+        public List<List<int>> tripLengthVariablesInAList()
+        {
+            List<List<int>> trip = new List<List<int>>();
+            trip.Add(kilometersTravelled);
+            trip.Add(hoursTravelled);
+            return trip;
+        }
+    }
+}
